aeddfc6d5d713943792c48c5d08ec1fa5c21368d
Eduardo Galvão <eduardotorresgalvao@gmail.com>
2021-03-20 11:42:57 -0300

    First commit

---
+++ electron/electron-practice-service/src/main/java/br/com/thor/ElectronPracticeServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.thor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronPracticeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronPracticeServiceApplication.class, args);
+	}
+
+}
---
+++ electron/electron-practice-service/src/test/java/br/com/thor/ElectronPracticeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronPracticeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
