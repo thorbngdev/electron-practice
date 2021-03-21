5071cdd0bee1b3aeb89ff908aa1c9f9fd3279dc9
Eduardo Galvão <eduardotorresgalvao@gmail.com>
2021-03-21 00:30:25 -0300

    Commit de ajuste do projeto

--- electron/electron-practice/src/app/app.component.ts
+++ electron/electron-practice/src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { map, takeWhile } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'electron-practice';
+    
+    max = 1;
+    current = 0;
+
+    start() {
+        interval(100).pipe(
+            takeWhile((_: any) => !this.isFinished),
+            map((_: any) => {
+                this.current += 0.1;
+            })
+        ).subscribe();
+    }
+
+    finish() {
+        this.current = this.max;
+    }
+
+    reset() {
+        this.current = 0;
+    }
+
+    get maxVal() {
+        return isNaN(this.max) || this.max < 0.1 ? 0.1 : this.max;
+    }
+
+    get currentVal() {
+        return isNaN(this.current) || this.current < 0 ? 0 : this.current;
+    }
+
+    get isFinished() {
+        return this.current >= this.max;
+    }
+
 }
